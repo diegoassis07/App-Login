36facbd1ba0b7af57383be112c8ce7b58bdc315d
Diego assis <diegoassis009@gmail.com>
2023-09-09 13:49:15 -0300

    Iniciando tela singIn

--- src/screens/auth/SiginIn/index.tsx
+++ src/screens/auth/SiginIn/index.tsx
@@ -1,7 +1,12 @@
-import { View, } from 'react-native'
+import { View, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import Logo from '../../../assets/Logo.png'
 
 export default function SignIn(){
     return (
-    <></>
+    <SafeAreaView>
+        <Image source={Logo}/>
+    </SafeAreaView>
     )
 }
