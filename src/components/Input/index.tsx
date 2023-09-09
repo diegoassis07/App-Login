2f5d5f1404926c044860135498a4631b3ae4b6c1
Gabriel Henrique <estudogbzs@gmail.com>
2023-09-09 18:50:47 -0300

    show password or not

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -10,13 +10,23 @@ import IconEyeOpenned from "../../assets/eye-openned.svg";
 import IconEyeClosed from "../../assets/eye-closed.svg";
 
 import * as S from "./style";
+import { useState } from "react";
 
 const getIcons = {
-  password: <IconPassword />,
+  password: <IconPassword style={{ marginLeft: 6 }} />,
   email: <IconEmail />,
 };
 
+const getIconEye = {
+  true: <IconEyeOpenned />,
+  false: <IconEyeClosed />,
+};
+
 const Input = ({ type, placeholder, title }: IInputProps) => {
+  const [secureTextEntry, setSecureTextEntry] = useState<boolean>(
+    type === "password"
+  );
+
   return (
     <S.Container>
       <S.Title>{title}</S.Title>
@@ -24,13 +34,18 @@ const Input = ({ type, placeholder, title }: IInputProps) => {
       <S.ContainerInput>
         {getIcons[type]}
         <S.Input
-          style={{ width: type === "password" ? "83%" : "90%" }}
+          style={{
+            width: type === "password" ? "83%" : "90%",
+            paddingLeft: type === "password" ? 8 : 5,
+          }}
           placeholder={placeholder}
-          secureTextEntry={type === "password"}
+          secureTextEntry={secureTextEntry}
         />
 
-        <S.HidePaswword>
-          {type === "password" && <IconEyeOpenned />}
+        <S.HidePaswword
+          onPress={() => setSecureTextEntry((previousState) => !previousState)}
+        >
+          {type === "password" && getIconEye[`${secureTextEntry}`]}
         </S.HidePaswword>
       </S.ContainerInput>
     </S.Container>
--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -13,16 +13,16 @@ export const ContainerInput = styled.View`
   width: 100%;
   height: 55px;
   border-radius: 10px;
+  padding: 5px;
   align-items: center;
   flex-direction: row;
-  padding-left: 10px;
+
   padding-right: 10px;
   justify-content: center;
 `;
 
 export const Input = styled.TextInput`
   font-size: 18px;
-  padding-left: 5px;
   padding-left: 5px;
 `;
 
