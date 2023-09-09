361479cdbbb225ec9acc2b123789e8b236259dd2
Gabriel Henrique <estudogbzs@gmail.com>
2023-09-09 13:53:50 -0300

    change path styled-components

--- src/screens/auth/SiginIn/index.tsx
+++ src/screens/auth/SiginIn/index.tsx
@@ -1,12 +1,12 @@
-import { View, Image } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import Logo from '../../../assets/Logo.png'
+import Logo from "../../../assets/Logo.png";
 
-export default function SignIn(){
-    return (
+export default function SignIn() {
+  return (
     <SafeAreaView>
-        <Image source={Logo}/>
+      <Image source={Logo} />
     </SafeAreaView>
-    )
+  );
 }
